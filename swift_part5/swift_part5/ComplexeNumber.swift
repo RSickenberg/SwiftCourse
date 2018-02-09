426f76ce9b9ba3927d7a3922f7f22524d8878c39
Romain Sickenberg <romain.sickenberg@liip.ch>
2018-02-09 15:24:07 +0100

    WIP: part5

---
+++ swift_part5/swift_part5/ComplexeNumber.swift
@@ -0,0 +1,77 @@
+//
+//  ComplexeNumber.swift
+//  swift_part3
+//
+//  Created by Romain Sickenberg on 09.02.18.
+//  Copyright © 2018 Romain Sickenberg. All rights reserved.
+//
+
+import Foundation
+
+protocol ComplexeLibraryDefProtocol {
+    func printComplex(_ complexNumberToPrint: ComplexeNumber)
+}
+
+class ComplexeNumber: NSObject {
+    
+    // MARK: Description
+    
+    override var description: String {
+        
+        get {
+            let newDescription : String = String(format: "Real Part is %f, Imaginary part is %f", realPart, imaginaryPart)
+            return newDescription
+        }
+    }
+    
+    // MARK: - INIT
+    
+    var realPart: Float
+    var imaginaryPart: Float
+    
+    override init() {
+        self.realPart = 0
+        self.imaginaryPart = 0
+        
+        super.init()
+    }
+    
+    convenience init(realPart: Float, imaginaryPart: Float) {
+        self.init()
+        
+        self.realPart = realPart
+        self.imaginaryPart = imaginaryPart
+        
+        print("Object ok.")
+    }
+    
+    convenience init(realPart: Float) {
+        self.init(realPart: realPart, imaginaryPart: 0)
+        
+        print("Object ok")
+    }
+    
+    // MARK: - OPERATIONS
+    
+    func clear() {
+        self.realPart = 0.0
+        self.imaginaryPart = 0.0
+    }
+    
+    func add(_ complexeNumber: ComplexeNumber) -> ComplexeNumber {
+        
+        // Initialized with Optionals:
+        self.realPart = self.realPart + complexeNumber.realPart
+        self.imaginaryPart = self.imaginaryPart + complexeNumber.realPart
+        
+        return self
+    }
+    
+    func printComplex() {
+//        if self.delegate != nil {
+//            self.delegate!.printComplex(self)
+//        } else {
+            print(String(format: "The complex number is: ((%.1f + %.1f))", self.realPart, self.imaginaryPart))
+//        }
+    }
+}
---
+++ swift_part5/swift_part5/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  swift_part5
+//
+//  Created by Romain Sickenberg on 09.02.18.
+//  Copyright © 2018 Romain Sickenberg. All rights reserved.
+//
+
+import Foundation
+
+var complexOne = ComplexeNumber()
+
+complexOne.printComplex()
